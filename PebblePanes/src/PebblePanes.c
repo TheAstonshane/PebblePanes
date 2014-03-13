62b6b2c6418f5b87bf4aa22bf0a60fedc8b7e615
Shane Aston <astonshane@gmail.com>
2014-03-13 17:15:15 -0400

    modified PebblePanes.c to make the pane names more descriptive and easy to understand. ie. made pane0_text_layer --> time_text_layer

--- PebblePanes/src/PebblePanes.c
+++ PebblePanes/src/PebblePanes.c
@@ -2,10 +2,10 @@
 
 static Window *window;
 
-static TextLayer *pane0_text_layer;
-static TextLayer *pane00_text_layer;
-static TextLayer *pane1_text_layer;
-static TextLayer *pane2_text_layer;
+static TextLayer *time_text_layer;
+static TextLayer *date_text_layer;
+static TextLayer *temp_text_layer;
+static TextLayer *weather_text_layer;
 static TextLayer *battery_layer;
 static BitmapLayer *icon_layer;
 static GBitmap *icon_bitmap = NULL;
@@ -50,11 +50,11 @@ static void sync_tuple_changed_callback(const uint32_t key, const Tuple* new_tup
 
     case WEATHER_TEMPERATURE_KEY:
       // App Sync keeps new_tuple in sync_buffer, so we may use it directly
-      text_layer_set_text(pane1_text_layer, new_tuple->value->cstring);
+      text_layer_set_text(temp_text_layer, new_tuple->value->cstring);
       break;
 
     case WEATHER_TYPE_KEY:
-      text_layer_set_text(pane2_text_layer, new_tuple->value->cstring);
+      text_layer_set_text(weather_text_layer, new_tuple->value->cstring);
       break;
   }
 }
@@ -86,42 +86,42 @@ static void handle_battery() {
 }
 
 //Pane loading////////////////////////////////////////////////////////////////////////
-static void pane0_text_load(TextLayer *pane0_text_layer, Layer *window_layer) {
-  //text_layer_set_text(pane0_text_layer, "tmp");
-  text_layer_set_text_color(pane0_text_layer, GColorWhite); 
-  text_layer_set_background_color(pane0_text_layer, GColorClear);
-  text_layer_set_text_alignment(pane0_text_layer, GTextAlignmentCenter);
-  text_layer_set_font(pane0_text_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_LIGHT));
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(pane0_text_layer));
+static void time_text_load(TextLayer *time_text_layer, Layer *window_layer) {
+  //text_layer_set_text(time_text_layer, "tmp");
+  text_layer_set_text_color(time_text_layer, GColorWhite); 
+  text_layer_set_background_color(time_text_layer, GColorClear);
+  text_layer_set_text_alignment(time_text_layer, GTextAlignmentCenter);
+  text_layer_set_font(time_text_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_LIGHT));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(time_text_layer));
 }
 
-static void pane00_text_load(TextLayer *pane00_text_layer, Layer *window_layer) {
-  //text_layer_set_text(pane00_text_layer, "Test");
-  text_layer_set_text_alignment(pane00_text_layer, GTextAlignmentCenter);
-  text_layer_set_background_color(pane00_text_layer, GColorClear);
-  text_layer_set_text_color(pane00_text_layer, GColorWhite);
-  text_layer_set_font(pane00_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28));
-  layer_add_child(window_layer, text_layer_get_layer(pane00_text_layer));
+static void date_text_load(TextLayer *date_text_layer, Layer *window_layer) {
+  //text_layer_set_text(date_text_layer, "Test");
+  text_layer_set_text_alignment(date_text_layer, GTextAlignmentCenter);
+  text_layer_set_background_color(date_text_layer, GColorClear);
+  text_layer_set_text_color(date_text_layer, GColorWhite);
+  text_layer_set_font(date_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28));
+  layer_add_child(window_layer, text_layer_get_layer(date_text_layer));
 
   //we don't want this layer to show up all of the time, only once the up button has been pressed
-  layer_set_hidden((Layer*) pane00_text_layer, true);
+  layer_set_hidden((Layer*) date_text_layer, true);
 }
-static void pane1_text_load(TextLayer *pane1_text_layer, Layer *window_layer) {
-  //text_layer_set_text(pane1_text_layer, "14\u00B0 ");
-  text_layer_set_text_alignment(pane1_text_layer, GTextAlignmentRight);
-  text_layer_set_background_color(pane1_text_layer, GColorClear);
-  text_layer_set_text_color(pane1_text_layer, GColorWhite);
-  text_layer_set_font(pane1_text_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_LIGHT));
-  layer_add_child(window_layer, text_layer_get_layer(pane1_text_layer));
+static void temp_text_load(TextLayer *temp_text_layer, Layer *window_layer) {
+  //text_layer_set_text(temp_text_layer, "14\u00B0 ");
+  text_layer_set_text_alignment(temp_text_layer, GTextAlignmentRight);
+  text_layer_set_background_color(temp_text_layer, GColorClear);
+  text_layer_set_text_color(temp_text_layer, GColorWhite);
+  text_layer_set_font(temp_text_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_LIGHT));
+  layer_add_child(window_layer, text_layer_get_layer(temp_text_layer));
 }
 
-static void pane2_text_load(TextLayer *pane2_text_layer, Layer *window_layer) {
-  //text_layer_set_text(pane2_text_layer, "");
-  text_layer_set_text_alignment(pane2_text_layer, GTextAlignmentCenter);
-  text_layer_set_background_color(pane2_text_layer, GColorClear);
-  text_layer_set_text_color(pane2_text_layer, GColorWhite);
-  text_layer_set_font(pane2_text_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_LIGHT));
-  layer_add_child(window_layer, text_layer_get_layer(pane2_text_layer));
+static void weather_text_load(TextLayer *weather_text_layer, Layer *window_layer) {
+  //text_layer_set_text(weather_text_layer, "");
+  text_layer_set_text_alignment(weather_text_layer, GTextAlignmentCenter);
+  text_layer_set_background_color(weather_text_layer, GColorClear);
+  text_layer_set_text_color(weather_text_layer, GColorWhite);
+  text_layer_set_font(weather_text_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_LIGHT));
+  layer_add_child(window_layer, text_layer_get_layer(weather_text_layer));
 }
 
 static void battery_layer_load(TextLayer *battery_layer, Layer *window_layer) {
@@ -140,27 +140,27 @@ static void battery_layer_load(TextLayer *battery_layer, Layer *window_layer) {
 //updating of time / date stuff//////////////////////////////////////////////////////////
 static void handle_minute_tick(struct tm* tick_time, TimeUnits units_changed) {
 
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "handle_minute_tick: setting time in pane0" );
-    //set pane0 - time
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "handle_minute_tick: setting time in time_text_layer" );
+    //set time - time
     static char time_text[] = "00:00"; // Needs to be static because it's used by the system later.
     if(clock_is_24h_style()){
       strftime(time_text, sizeof(time_text), "%H:%M", tick_time);
     }else{
       strftime(time_text, sizeof(time_text), "%l:%M", tick_time);
     }
-    text_layer_set_text(pane0_text_layer, time_text);
+    text_layer_set_text(time_text_layer, time_text);
 }
 
 static void handle_date_update(struct tm* tick_time, TimeUnits units_changed) {
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "date_update(): setting date in pane00" );
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "date_update(): setting date in date_text_layer" );
 
   static char time_text2[] = "###  ### ##"; // Needs to be static because it's used by the system later.
   strftime(time_text2, sizeof(time_text2), "%a %b %e", tick_time);
   if(time_text2[7] == ' '){
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "resizing date in pane00 due to single-digit date" );
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "resizing date in date_text_layer due to single-digit date" );
     strftime(time_text2, sizeof(time_text2), "%a  %b %e", tick_time);
    }
-  text_layer_set_text(pane00_text_layer, time_text2);
+  text_layer_set_text(date_text_layer, time_text2);
 }
 
 
@@ -190,25 +190,25 @@ static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
 }
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  if(!layer_get_hidden((Layer*) pane0_text_layer)) {
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "up click handler pressed, pane0_text_layer will now be hidden..." );
-    layer_set_hidden((Layer*) pane0_text_layer, true);
-    layer_set_hidden((Layer*) pane00_text_layer, false);
+  if(!layer_get_hidden((Layer*) time_text_layer)) {
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "up click handler pressed, time_text_layer will now be hidden..." );
+    layer_set_hidden((Layer*) time_text_layer, true);
+    layer_set_hidden((Layer*) date_text_layer, false);
   }else{
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "up click handler pressed, pane0_text_layer will now be UN-hidden..." );
-    layer_set_hidden((Layer*) pane0_text_layer, false);
-    layer_set_hidden((Layer*) pane00_text_layer, true);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "up click handler pressed, time_text_layer will now be UN-hidden..." );
+    layer_set_hidden((Layer*) time_text_layer, false);
+    layer_set_hidden((Layer*) date_text_layer, true);
   }
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  if(!layer_get_hidden((Layer*) pane2_text_layer)) {
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "up click handler pressed, pane0_text_layer will now be hidden..." );
-    layer_set_hidden((Layer*) pane2_text_layer, true);
+  if(!layer_get_hidden((Layer*) weather_text_layer)) {
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "down_click_handler click handler pressed, weather_text_layer will now be hidden..." );
+    layer_set_hidden((Layer*) weather_text_layer, true);
     layer_set_hidden((Layer*) battery_layer, false);
   }else{
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "up click handler pressed, pane0_text_layer will now be UN-hidden..." );
-    layer_set_hidden((Layer*) pane2_text_layer, false);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "down_click_handler click handler pressed, weather_text_layer will now be UN-hidden..." );
+    layer_set_hidden((Layer*) weather_text_layer, false);
     layer_set_hidden((Layer*) battery_layer, true);
   }
 }
@@ -242,23 +242,23 @@ static void window_load(Window *window) {
   icon_layer = bitmap_layer_create(GRect(5, 50, 55, 55));
   layer_add_child(window_layer, bitmap_layer_get_layer(icon_layer));
 
-  // Init pane0_layer // TIME
-  pane0_text_layer = text_layer_create((GRect) { .origin = { 0, 0 }, .size = { 144, 50 } });
-  pane0_text_load(pane0_text_layer, window_layer);
+  // Init time_text_layer // TIME
+  time_text_layer = text_layer_create((GRect) { .origin = { 0, 0 }, .size = { 144, 50 } });
+  time_text_load(time_text_layer, window_layer);
 
-  // Init pane00_layer // date
-  pane00_text_layer = text_layer_create((GRect) { .origin = { 0, 0 }, .size = { 144, 50 } });
-  pane00_text_load(pane00_text_layer, window_layer);
+  // Init date_text_layer // date
+  date_text_layer = text_layer_create((GRect) { .origin = { 0, 0 }, .size = { 144, 50 } });
+  date_text_load(date_text_layer, window_layer);
 
   update_time();
 
-  //pane1_text_layer = text_layer_create(GRect(0, 50, 144, 55));
-  pane1_text_layer = text_layer_create(GRect(0, 50, 135, 55));
+  //temp_text_layer = text_layer_create(GRect(0, 50, 144, 55));
+  temp_text_layer = text_layer_create(GRect(0, 50, 135, 55));
 
-  pane1_text_load(pane1_text_layer, window_layer);
+  temp_text_load(temp_text_layer, window_layer);
 
-  pane2_text_layer = text_layer_create(GRect(0, 105, 144, 168-50-55));
-  pane2_text_load(pane2_text_layer, window_layer);
+  weather_text_layer = text_layer_create(GRect(0, 105, 144, 168-50-55));
+  weather_text_load(weather_text_layer, window_layer);
 
   battery_layer = text_layer_create(GRect(0, 105, 144, 168-50-55));
   battery_layer_load(battery_layer, window_layer);
@@ -273,10 +273,10 @@ static void window_unload(Window *window) {
     gbitmap_destroy(icon_bitmap);
   }
   text_layer_destroy(battery_layer);
-  text_layer_destroy(pane2_text_layer);
-  text_layer_destroy(pane1_text_layer);
-  text_layer_destroy(pane00_text_layer);
-  text_layer_destroy(pane0_text_layer);
+  text_layer_destroy(weather_text_layer);
+  text_layer_destroy(temp_text_layer);
+  text_layer_destroy(date_text_layer);
+  text_layer_destroy(time_text_layer);
   bitmap_layer_destroy(icon_layer);
 }
 
